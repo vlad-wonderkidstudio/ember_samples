ea04ce0eea90eee7cc0dbe135ce1e04b3c4797d4
Vlad <vlad@wonderkidstudio.com>
2017-12-07 02:26:12 +0200

    signed the data up via REST API (need to make it using JSON API now)

--- rentals/app/adapters/application.js
+++ rentals/app/adapters/application.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+//export default DS.JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
+  namespace: 'admin_panel/ember_samples/rentals_yii/yii2-advanced-api/api/web/rental',
+  host: 'http://ember.loc'
 });
